223bf368f06fb84b5caef0e69003fd4770d2da87
felix.ravella <felix.ravella@gmail.com>
2023-03-10 10:10:53 +0100

    feat: basic useRef PoC

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 
+import { useRef } from 'react';
 import './App.css';
 
 function App(){
+
+  const inputRef = useRef(null)
+
+  function handleClick(){
+    console.log('clicked')
+    inputRef.current.value = "";
+  }
+
   return (
     <div className='App'>
-      <h2>APP</h2>
+      <h2>Demo useRef</h2>
+      <input type="text" ref={inputRef} />
+      <button type="button" onClick={() => handleClick()}> Reset input </button>
     </div>
   )
 }
