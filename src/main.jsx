90af361f0f1899f80d10f76f887dfa8fae10b078
felix.ravella <felix.ravella@gmail.com>
2023-03-10 09:57:35 +0100

    inital commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import './index.css'
+
+
+import Kanban from './views/Kanban'
+
+const router = createBrowserRouter([
+
+  {
+    path: '/',
+    element: <AppLayout/>,
+    children: [
+      {
+        path: '/',
+        element: <Home/>
+      },
+      {
+        path: '/kanban',
+        element: <Kanban/>
+      }
+    ]
+  }
+
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
